89f3a4aa3955c06099f68f8fabb2cbec65cbeed0
jchassi1 <jchassi1@binghamton.edu>
2025-02-16 15:15:24 -0500

    Created another script to explore Financial Disclosures API

---
+++ src/analyze_gifts_of_judges.py
@@ -0,0 +1,14 @@
+#https://www.courtlistener.com/help/api/rest/financial-disclosures/
+from api import query_api, query_options, people_ep
+import pandas as pd
+
+#Current Supreme Court justices
+#https://www.supremecourt.gov/about/biographies.aspx
+scotus_judges = ['John Glover Roberts', 'Clarence Thomas', 'Samuel Anthony Alito', 'Sonia Sotomayor',
+                 'Elena Kagan', 'Neil McGill Gorsuch', 'Brett Michael Kavanaugh', 'Amy Coney Barrett',
+                 'Ketanji Brown Jackson']
+
+if __name__=='__main__':
+    #Get ids of judges
+    for judge in scotus_judges:
+        name = scotus_judges.split(" ")
